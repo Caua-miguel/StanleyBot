ee5ba6249f8b9f41c0376fad97b6b024f8e963b8
Cauã Miguel Sudol Rocha <132935686+Caua-miguel@users.noreply.github.com>
2024-04-14 19:05:25 -0300

    Add files via upload

--- src/main/java/me/cauadeveloper/bot/StanleyBot.java
+++ src/main/java/me/cauadeveloper/bot/StanleyBot.java
@@ -1,5 +1,6 @@
 package me.cauadeveloper.bot;
 
+import me.cauadeveloper.commands.createRole;
 import me.cauadeveloper.commands.goodNight;
 import me.cauadeveloper.commands.ping;
 import net.dv8tion.jda.api.JDA;
@@ -11,9 +12,10 @@ import java.util.EnumSet;
 public class StanleyBot {
 
     public static void main(String[] args) throws Exception{
-        JDA jda = JDABuilder.createDefault("",
+        JDA jda = JDABuilder.createDefault("MTIyNjY1MzQyMzE4MTYzMTY0OQ.GRTVLS.s848HRg8AwKCVxBa11CvNBy428cbnAlUbpZ_HA",
         EnumSet.allOf(GatewayIntent.class)).build();
         jda.addEventListener(new ping());
         jda.addEventListener(new goodNight());
+        jda.addEventListener(new createRole());
     }
 }
