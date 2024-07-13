36f30064cebac0861c4c2ce7eec2a0bb2a7cfee3
caua <caua.sudol@outlook.com>
2024-07-12 22:50:47 -0300

    feat: readConfig pass

--- StanleyBot/src/test/java/readConfigTest.java
+++ StanleyBot/src/test/java/readConfigTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static me.cauadeveloper.database.dataconfig.readConfig.dataUserCollumnA;
+import static me.cauadeveloper.database.dataconfig.readConfig.*;
 
 public class readConfigTest {
 
@@ -14,6 +14,32 @@ public class readConfigTest {
 
         String[] saidaArr = dataUserCollumnA();
         String[] saidaEsperadaArr = {"caua", "lucas", "brendo"};
+
+        System.out.println(Arrays.toString(saidaArr));
+
+        Assertions.assertEquals(Arrays.toString(saidaEsperadaArr), Arrays.toString(saidaArr));
+
+    }
+
+    @DisplayName("Saida esperada Da Operacão De Transformar Lista Em Array Na Coluna E")
+    @Test
+    public void saidaEsperadaTranformandoListaEmArrayCollumnE(){
+
+        String[] saidaArr = dataUserCollumnE();
+        String[] saidaEsperadaArr = {"coaLegal", "wash", "trocaFill"};
+
+        System.out.println(Arrays.toString(saidaArr));
+
+        Assertions.assertEquals(Arrays.toString(saidaEsperadaArr), Arrays.toString(saidaArr));
+
+    }
+
+    @DisplayName("Saida esperada Da Operacão De Transformar Lista Em Array Na Coluna I")
+    @Test
+    public void saidaEsperadaTranformandoListaEmArrayCollumnI(){
+
+        String[] saidaArr = dataUserCollumnI();
+        String[] saidaEsperadaArr = {"teste", "teste123", "teste teste"};
 
         System.out.println(Arrays.toString(saidaArr));
 
