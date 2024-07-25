1ef515902dd4d0a109ac0c4c022b566b2debe6ab
caua <caua.sudol@outlook.com>
2024-07-25 19:33:02 -0300

    feat: timer

--- StanleyBot/src/main/java/me/cauadeveloper/commands/general/timer.java
+++ StanleyBot/src/main/java/me/cauadeveloper/commands/general/timer.java
@@ -1,67 +1,30 @@
 package me.cauadeveloper.commands.general;
 
-import me.cauadeveloper.utils.timer.Minuto;
-import me.cauadeveloper.utils.timer.Segundo;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.utils.messages.MessageCreateData;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
 import static java.util.concurrent.TimeUnit.*;
-import static me.cauadeveloper.utils.fixValues.utilsStaticMethods.txtMin;
-import static me.cauadeveloper.utils.fixValues.utilsStaticMethods.txtSeg;
-
 public class timer extends ListenerAdapter {
 
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if(event.getAuthor().isBot()) return;
 
-        Message message = event.getMessage();
-        String content = message.getContentRaw();
-        MessageChannel channel = event.getChannel();
-        EmbedBuilder embed = new EmbedBuilder();
-        final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        String[] command = event.getMessage().getContentRaw().split(" ", 2);
 
-        Minuto minuto;
-        Segundo segundo;
-        Thread tMin, tSeg;
+        if(command[0].equalsIgnoreCase("!StartCoffe")) {
+            if (command.length < 2){
 
+                // Deixar a mensagem mais clara !!!
+                event.getChannel().sendMessage("digite o parametro!!!").queue();
 
-        if(content.equalsIgnoreCase("!StartCoffe")) {
+            }else{
 
-            //Timer
-            minuto = new Minuto();
-            tMin = new Thread(minuto);
+                String text = "Seu café estará pronto em " + command[1] + " minutos...";
 
-            segundo = new Segundo();
-            tSeg = new Thread(segundo);
-
-
-            String text = "Seu café estara pronto em 6 minutos...";
-
-
-            Runnable timer = () -> channel.sendMessage(text).queue(sentMessage -> {
-                sentMessage.editMessage("Seu café está pronto!").queueAfter(10, SECONDS);
-            });
-
-            ScheduledFuture timerHandle = scheduler.schedule(timer, 0, SECONDS);
-            Runnable canceller = () -> timerHandle.cancel(false);
-            scheduler.schedule(canceller, 30, SECONDS);
-
-              //Mensagem
-//            embed.setTitle("Cronometro");
-//            embed.setDescription("Timer: " + txtMin + ":" + txtSeg);
-//            channel.sendMessageEmbeds(embed.build()).queue();
-
+                event.getChannel().sendMessage(text).queue(sentMessage -> {
+                    sentMessage.editMessage("Seu café está pronto!").queueAfter(Long.parseLong(command[1]), MINUTES);
+                });
+            }
         }
     }
 
