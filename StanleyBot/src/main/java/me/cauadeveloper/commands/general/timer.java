2f260e46f513995425a11ddfbd1692cbc2e69e72
caua <caua.sudol@outlook.com>
2024-07-25 22:50:03 -0300

    feat: timer

--- StanleyBot/src/main/java/me/cauadeveloper/commands/general/timer.java
+++ StanleyBot/src/main/java/me/cauadeveloper/commands/general/timer.java
@@ -11,32 +11,48 @@ public class timer extends ListenerAdapter {
 
         String[] command = event.getMessage().getContentRaw().split(" ", 2);
 
-        if(command[0].equalsIgnoreCase("!StartCoffe")) {
 
-            String text = "Seu café estará pronto em " + command[1] + " minutos...";
-            int minutos = Integer.parseInt(command[1]);
+        try {
 
-            if (command.length < 2){
+            if(command[0].equalsIgnoreCase("!StartCoffe")) {
 
-                event.getChannel().sendMessage(text).queue(sentMessage -> {
-                    sentMessage.editMessage("Seu café está pronto!").queueAfter(10, MINUTES);
-                });
+                String text = "Seu café estará pronto em " + command[1] + " minutos...";
+                int minutos = Integer.parseInt(command[1]);
 
-            }if(minutos <= 30){
+                if(minutos <= 30 && minutos != 0){
 
-                event.getChannel().sendMessage(text).queue(sentMessage -> {
-                    sentMessage.editMessage("Seu café está pronto!").queueAfter(Long.parseLong(command[1]), MINUTES);
-                });
+                    event.getChannel().sendMessage(text).queue(sentMessage -> {
+                        sentMessage.editMessage("Seu café está pronto!").queueAfter(Long.parseLong(command[1]), MINUTES);
+                    });
 
-            }else{
+                }else if(minutos == 0){
+                    event.getChannel().sendMessage("Não estava esperando que você fosse tão rápido!!! Vou te avisar daqui 1 minuto...").queue(
+                            sentMessage ->{
+                                sentMessage.editMessage("Seu café está pronto!").queueAfter(1, MINUTES);
+                            }
+                    );
+                }else{
 
-                event.getChannel().sendMessage("Mais de 30 minutos para fazer um café? Não precisa de tanto, eu confio em você!!!\nVocê tem 10 minutos!").queue(
-                        sentMessage -> {
-                            sentMessage.editMessage("Seu café está pronto!").queueAfter(10, MINUTES);
-                        });
+                    event.getChannel().sendMessage("Mais de 30 minutos para fazer um café? Não precisa de tanto, eu confio em você!!!\nVocê tem 10 minutos!").queue(
+                            sentMessage -> {
+                                sentMessage.editMessage("Seu café está pronto!").queueAfter(10, MINUTES);
+                            });
 
+                }
             }
+
+        }catch (NumberFormatException e){
+
+            event.getChannel().sendMessage("Você precisa digitar um numero interino logo depois de rodar o comando <!startCoffe> <numero inteiro>." +
+                    "\nExemplo: !startCoffe 2\n Para ser lembrado após 2 minutos. Rode o comando novamente, porfavor!").queue();
+
+        }catch(ArrayIndexOutOfBoundsException e){
+
+            event.getChannel().sendMessage("Seu café estará pronto em 10 minutos...").queue(sentMessage -> {
+                sentMessage.editMessage("Seu café está pronto!").queueAfter(10, MINUTES);
+            });
         }
+
     }
 
 }
